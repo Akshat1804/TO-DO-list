0d410a9c1639e3b93a60feb6f3dbc430e37d71bc
Akshat1804 <86612849+Akshat1804@users.noreply.github.com>
2024-12-18 21:25:02 +0530

    Update Create.jsx

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -25,7 +25,7 @@ const Create = (props) => {
       className="w-1/2 h-[10%] bg-slate-800 flex items-center px-4  mt-[11%] absolute rounded-2xl shadow-lg shadow-slate-500"
     >
       <input
-        className="w-[80%] h-[30px] ml-7 p-6 rounded-md bg-transparent border-b-2 border-slate-300 "
+        className="w-[80%] h-[30px] ml-7 text-white p-6 rounded-md bg-transparent border-b-2 border-slate-300 "
         type="text"
         placeholder="Enter your task"
         value={title}
